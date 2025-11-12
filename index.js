46b060dd1b7e5c3fad3bec59fe9d19703aaf37e2
Ibrahim Fahad <Ibrazoom68@gmail.com>
2025-11-12 21:45:10 +0300

    add getEmbedding

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-import { openai, supabase } from './config.js';
+import { openai, supabase } from "./config.js";
+import { movies } from "./content.js";
+
+async function getEmbedding(input) {
+  const embedding = await openai.embeddings.create({
+    model: "text-embedding-3-small",
+    input: input,
+  });
+  return embedding;
+}
+
+async function storeEmbeddings() {
+  const embbedings = await getEmbedding(movies.map((m) => m.content));
+}
