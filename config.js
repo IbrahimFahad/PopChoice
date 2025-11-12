73abc27d9834990000db3aa4d6726042dc0fd693
Ibrahim Fahad <Ibrazoom68@gmail.com>
2025-11-12 21:15:49 +0300

    html and css

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
-import OpenAI from 'openai';
+import OpenAI from "openai";
 import { createClient } from "@supabase/supabase-js";
 
 /** OpenAI config */
-if (!process.env.OPENAI_API_KEY) throw new Error("OpenAI API key is missing or invalid.");
+if (!process.env.OPENAI_API_KEY)
+  throw new Error("OpenAI API key is missing or invalid.");
 export const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
-  dangerouslyAllowBrowser: true
+  dangerouslyAllowBrowser: true,
 });
 
 /** Supabase config */
